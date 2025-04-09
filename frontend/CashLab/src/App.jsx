c51bb9d9d38e8d50017d4a6771a5e4504de013ff
Pahan <pahanbro21@gmail.com>
2025-04-09 17:22:40 +0530

    initial commit

---
+++ frontend/CashLab/src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expenses from "./pages/Dashboard/Expense";
+import UserProvider from './context/userContext';
+
+const App = () => {
+  return (
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/SignUp" exact element={<SignUp />} />
+            <Route path="/dashboard" exact element={<Home />} />
+            <Route path="/Income" exact element={<Income />} />
+            <Route path="/Expenses" exact element={<Expenses />} />
+          </Routes>
+        </Router>
+      </div>
+    </UserProvider>
+  )
+}
+
+export default App
+
+const Root = () => {
+  //Check if token exists in localstorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ):(
+    <Navigate to="/login" />
+  );
+};
---
+++ frontend/CashLab/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,140 @@
+import React from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/input';
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useContext } from 'react';
+import { UserContext } from '../../context/userContext';
+import uploadImage from '../../utils/uploadimage';
+
+
+const SignUp = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const {updateUser} = useContext(UserContext)
+
+  const navigate = useNavigate();
+
+  //Handle SignUp From Submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Please Enter your Name");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the Password.");
+      return;
+    }
+
+    setError("");
+
+    //SignUp API call
+    try {
+
+      // Upload image if present
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create a Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+
+        <form onSubmit={handleSignUp}>
+
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="Kamal"
+              type="text"
+            />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="nsbm@example.com"
+              type="text"
+            />
+            <div className="col-span-2">
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                type="password"
+              />
+            </div>
+          </div>
+
+          
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          
+            <button type="submit" className="bt-primary">
+              SignUp
+            </button>
+          
+            <p className="text-[13px] text-slate-800 mt-3">
+              Already have an Account?{" "}
+                <Link className="font-medium text-red underline" to="/login">
+                  Login
+                </Link>
+             </p>
+        </form>
+      </div>
+    </AuthLayout>
+  )
+}
+
+export default SignUp
